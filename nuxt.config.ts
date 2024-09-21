4f4af6c442e76bdc2377000dd96a90257a47255f
Cenk Bülbül <cenkbulbul67@gmail.com>
2024-09-22 00:56:53 +0300

    feat: added nuxticon/nuxt-google-font packages, shadcn button/card/select/input components

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "@nuxtjs/i18n"],
   devtools: { enabled: false },
+  compatibilityDate: "2024-09-20",
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+    "@nuxtjs/i18n",
+    "@nuxt/icon",
+    [
+      "@nuxtjs/google-fonts",
+      {
+        families: {
+          GMSans: true,
+          Poppins: true,
+        },
+      },
+    ],
+    "@nuxt/icon",
+  ],
+  css: ["@/assets/styles/index.scss"],
   shadcn: {
     /**
      * Prefix for all the imported component
@@ -16,5 +33,4 @@ export default defineNuxtConfig({
   i18n: {
     vueI18n: "./i18n.config.ts", // if you are using custom path, default
   },
-  compatibilityDate: "2024-09-20",
 });
