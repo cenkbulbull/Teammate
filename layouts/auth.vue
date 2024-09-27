9e5433bc394114f83d2e59d2ac998b494a780483
Cenk Bülbül <cenkbulbul67@gmail.com>
2024-09-27 20:37:05 +0300

    fix: layout meta moved to appvue

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,20 +1,3 @@
-<script lang="ts" setup>
-const { locale, setLocale, t } = useI18n();
-useHead({
-  meta: [
-    {
-      name: "description",
-      content: t("projectGlobalDescription"),
-    },
-    {
-      name: "keywords",
-      content: t("projectGlobalKeywords"),
-    },
-    { name: "author", content: "Cenk Bülbül" },
-  ],
-});
-</script>
-
 <template>
   <div>
     <NuxtPage />
