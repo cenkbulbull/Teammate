f3db886514d1bb8197d2be6e4be92cbd82290319
Cenk Bülbül <cenkbulbul67@gmail.com>
2024-10-17 19:20:16 +0300

    fix: deploy error resolution test

--- app.config.ts
+++ app.config.ts
@@ -1,4 +1,5 @@
 export default defineAppConfig({
+  NUXT_BASE_URL: process.env.NUXT_BASE_URL,
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
   AUTH_SECRET: process.env.AUTH_SECRET,
--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -25,9 +25,7 @@ export default NuxtAuthHandler({
       async authorize(credentials: any) {
         let users = null;
         try {
-          const response = await fetch(
-            "http://localhost:3000/api/users/getUsers"
-          );
+          const response = await fetch(appConfig.NUXT_BASE_URL);
           if (!response.ok) {
             throw new Error("Failed to fetch users");
           }
