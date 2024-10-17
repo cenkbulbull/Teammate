2415f8bad3f96017d2d87825d3d37436b119896b
Cenk Bülbül <cenkbulbul67@gmail.com>
2024-10-17 18:26:24 +0300

    fix: deploy error resolution test

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -7,6 +7,7 @@ import { v4 as uuidv4 } from "uuid";
 const appConfig = useAppConfig();
 
 export default NuxtAuthHandler({
+  secret: process.env.AUTH_SECRET,
   pages: {
     signIn: "/auth/Login",
   },
